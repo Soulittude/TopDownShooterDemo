e5dac9798a78df961b636eae114892e1693ee688
soulittude <79145776+Soulittude@users.noreply.github.com>
2025-01-10 00:34:07 +0300

    Movement With Input System + Player Sprites

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField]
+    private float moveSpeed;
+    private Rigidbody2D rb;
+    private Vector2 _movementInput;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        rb.velocity = _movementInput * moveSpeed;
+    }
+
+    private void OnMove(InputValue inputValue)
+    {
+        _movementInput = inputValue.Get<Vector2>();
     }
 }
