24c8af75f2204e1ca11a1f2e959936eaeb3466b8
soulittude <79145776+Soulittude@users.noreply.github.com>
2025-01-10 20:46:03 +0300

    Rotate according to movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,8 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField]
     private float moveSpeed;
+    [SerializeField]
+    private float rotationSpeed;
     private Rigidbody2D rb;
     private Vector2 _movementInput;
     private Vector2 _smoothedMovementInput;
@@ -19,12 +21,28 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     private void FixedUpdate()
     {
-        _smoothedMovementInput = Vector2.SmoothDamp(_smoothedMovementInput, _movementInput, ref _smoothedMovementInput, 0.1f);
-        rb.velocity = _smoothedMovementInput * moveSpeed;
+        SetPlayerVelocity();
+        RotateInDirectionOfInput();
+
     }
 
     private void OnMove(InputValue inputValue)
     {
         _movementInput = inputValue.Get<Vector2>();
+    }
+
+    private void RotateInDirectionOfInput(){
+        if(_movementInput != Vector2.zero){
+            Quaternion targetRotation = Quaternion.LookRotation(transform.forward, _smoothedMovementInput);
+            Quaternion rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+        
+            rb.MoveRotation(rotation);
+        }
+    }
+
+    private void SetPlayerVelocity()
+    {
+        _smoothedMovementInput = Vector2.SmoothDamp(_smoothedMovementInput, _movementInput, ref _smoothedMovementInput, 0.1f);
+        rb.velocity = _smoothedMovementInput * moveSpeed;
     }
 }
