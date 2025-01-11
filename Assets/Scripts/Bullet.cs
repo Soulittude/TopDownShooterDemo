096db122633ea670c93f58531b1f4ad22af65393
soulittude <79145776+Soulittude@users.noreply.github.com>
2025-01-12 01:41:19 +0300

    Destroy Bullet If Offscreen

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,10 +4,29 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    private Camera mainCamera;
+
+    private void Awake(){
+        mainCamera = Camera.main;
+    }
+
+    private void Update(){
+        DestroyWhenOffScreen();
+    }
     // Start is called before the first frame update
     private void OnTriggerEnter2D(Collider2D collision){
         if(collision.GetComponent<EnemyMovement>()){
             Destroy(collision.gameObject);
+            Destroy(gameObject);
+        }
+    }
+
+    private void DestroyWhenOffScreen(){
+        Vector2 screenPosition = mainCamera.WorldToScreenPoint(transform.position);
+        if(screenPosition.x < 0 || 
+        screenPosition.x > mainCamera.pixelWidth || 
+        screenPosition.y < 0 || 
+        screenPosition.y > mainCamera.pixelHeight){
             Destroy(gameObject);
         }
     }
