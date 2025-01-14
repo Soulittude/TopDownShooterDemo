e1de1be44e4acc1253a377458b7144b76642e3a7
soulittude <79145776+Soulittude@users.noreply.github.com>
2025-01-14 23:58:02 +0300

    AI Obstacle Avoidance

--- Assets/Scripts/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -5,29 +5,41 @@ using UnityEngine;
 public class EnemyMovement : MonoBehaviour
 {
     [SerializeField]
-    private float moveSpeed;
+    private float speed;
 
     [SerializeField]
     private float rotationSpeed;
 
     [SerializeField]
     private float screenBorder;
+
+    [SerializeField]
+    private float obstacleCheckCircleRadius;
+
+    [SerializeField]
+    private float obstacleCheckDistance;
+
+    [SerializeField]
+    private LayerMask obstacleLayerMask;
+
     private Rigidbody2D rb;
-    private PlayerAwarenessController _playerAwarenessController;
-    private Vector2 _targetDirection;
+    private PlayerAwarenessController playerAwarenessController;
+    private Vector2 targetDirection;
     private float changeDirectionCooldown;
     private Camera mainCamera;
+    private RaycastHit2D[] obstacleCollisions;
+    private float obstacleAvoidanceCooldown;
+    private Vector2 obstacleAvoidanceTargetDirection;
 
-    // Start is called before the first frame update
     private void Awake()
     {
-        rb  = GetComponent<Rigidbody2D>();
-        _playerAwarenessController = GetComponent<PlayerAwarenessController>();
-        _targetDirection = transform.up;
+        rb = GetComponent<Rigidbody2D>();
+        playerAwarenessController = GetComponent<PlayerAwarenessController>();
+        targetDirection = transform.up;
         mainCamera = Camera.main;
+        obstacleCollisions = new RaycastHit2D[10];
     }
 
-    // Update is called once per frame
     private void FixedUpdate()
     {
         UpdateTargetDirection();
@@ -35,50 +47,101 @@ public class EnemyMovement : MonoBehaviour
         SetVelocity();
     }
 
-    private void UpdateTargetDirection(){
+    private void UpdateTargetDirection()
+    {
         HandleRandomDirectionChange();
         HandlePlayerTargeting();
+        HandleObstacles();
         HandleEnemyOffScreen();
     }
 
-    private void HandleRandomDirectionChange(){
+    private void HandleRandomDirectionChange()
+    {
         changeDirectionCooldown -= Time.deltaTime;
 
-        if(changeDirectionCooldown <= 0){
-            float angleChange = Random.Range(-90f, 90);
-            Quaternion rotation = Quaternion.AngleAxis(angleChange, Vector3.forward);
-            _targetDirection = rotation * _targetDirection;
+        if (changeDirectionCooldown <= 0)
+        {
+            float angleChange = Random.Range(-90f, 90f);
+            Quaternion rotation = Quaternion.AngleAxis(angleChange, transform.forward);
+            targetDirection = rotation * targetDirection;
 
             changeDirectionCooldown = Random.Range(1f, 5f);
         }
     }
 
-    private void HandlePlayerTargeting(){
-        if(_playerAwarenessController.AwareOfPlayer){
-            _targetDirection = _playerAwarenessController.DirectionToPlayer;
+    private void HandlePlayerTargeting()
+    {
+        if (playerAwarenessController.AwareOfPlayer)
+        {
+            targetDirection = playerAwarenessController.DirectionToPlayer;
         }
     }
 
-    private void HandleEnemyOffScreen(){
+    private void HandleEnemyOffScreen()
+    {
         Vector2 screenPosition = mainCamera.WorldToScreenPoint(transform.position);
 
-        if((screenPosition.x < screenBorder && _targetDirection.x < 0) || (screenPosition.x > mainCamera.pixelWidth - screenBorder && _targetDirection.x > 0)){
-            _targetDirection = new Vector2(_targetDirection.x * -1, _targetDirection.y);
+        if ((screenPosition.x < screenBorder && targetDirection.x < 0) ||
+            (screenPosition.x > mainCamera.pixelWidth - screenBorder && targetDirection.x > 0))
+        {
+            targetDirection = new Vector2(-targetDirection.x, targetDirection.y);
         }
 
-        if((screenPosition.y < screenBorder && _targetDirection.y < 0) || (screenPosition.y > mainCamera.pixelHeight - screenBorder && _targetDirection.y > 0)){
-            _targetDirection = new Vector2(_targetDirection.x, _targetDirection.y * -1);
+        if ((screenPosition.y < screenBorder && targetDirection.y < 0) ||
+            (screenPosition.y > mainCamera.pixelHeight - screenBorder && targetDirection.y > 0))
+        {
+            targetDirection = new Vector2(targetDirection.x, -targetDirection.y);
         }
     }
 
-    private void RotateTowardsTarget(){
-        Quaternion targetRotation = Quaternion.LookRotation(transform.forward, _targetDirection);
+    private void HandleObstacles()
+    {
+        obstacleAvoidanceCooldown -= Time.deltaTime;
+
+        var contactFilter = new ContactFilter2D();
+        contactFilter.SetLayerMask(obstacleLayerMask);
+
+        int numberOfCollisions = Physics2D.CircleCast(
+            transform.position,
+            obstacleCheckCircleRadius,
+            transform.up,
+            contactFilter,
+            obstacleCollisions,
+            obstacleCheckDistance);
+
+        for (int index = 0; index < numberOfCollisions; index++)
+        {
+            var obstacleCollision = obstacleCollisions[index];
+
+            if (obstacleCollision.collider.gameObject == gameObject)
+            {
+                continue;
+            }
+
+            if (obstacleAvoidanceCooldown <= 0)
+            {
+                obstacleAvoidanceTargetDirection = obstacleCollision.normal;
+                obstacleAvoidanceCooldown = 0.5f;
+            }
+
+            var targetRotation = Quaternion.LookRotation(transform.forward, obstacleAvoidanceTargetDirection);
+            var rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+
+            targetDirection = rotation * Vector2.up;
+            break;
+        }
+    }
+
+    private void RotateTowardsTarget()
+    {
+        Quaternion targetRotation = Quaternion.LookRotation(transform.forward, targetDirection);
         Quaternion rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
 
         rb.SetRotation(rotation);
     }
 
-    private void SetVelocity(){
-            rb.velocity = _targetDirection * moveSpeed;
+    private void SetVelocity()
+    {
+        rb.velocity = transform.up * speed;
     }
 }
